ad25b74e70e1b221e5074bb8c32bc431288af5c8
Masatoshi Nishimura <personal.trainer.bank@gmail.com>
2025-07-14 01:24:22 +0900

    Update page-landing.php 無駄なコメントアウトを全部削除

--- page-landing.php
+++ page-landing.php
@@ -4,75 +4,12 @@
  * Template Post Type: page
  * 
  * Made By Nissy 2025/7/4
- * Lightning子テーマ直下に配置する
+ * Lightning子テーマ直下に配置
  */
 
 // lightning\_g3\header.php
 lightning_get_template_part( 'header' );
 
-//不要do_action
-//do_action( 'lightning_site_header_before', 'lightning_site_header_before' );
-
-//ロゴとグロナビ を false で読み込み禁止
-/*
-if ( apply_filters( 'lightning_is_site_header', false, 'site_header' ) ) {
-	lightning_get_template_part( 'template-parts/site-header' );
-}
-*/
-//不要do_action
-//do_action( 'lightning_site_header_after', 'lightning_site_header_after' );
-
-?>
-
-<? //ここから lightning\_g3\index.php ?>
-
-<?php // if ( ! is_front_page() ) : ?>
-
-	<?php
-    //不要do_action
-	//do_action( 'lightning_page_header_before', 'lightning_page_header_before' );
-
-	//固定ページ先頭のWideな帯文字をfalseで読み込み禁止
-/*
-	if ( apply_filters( 'lightning_is_page_header', false, 'page_header' ) ) {
-		lightning_get_template_part( 'template-parts/page-header' );
-	}
-*/
-    //不要do_action
-	//do_action( 'lightning_page_header_after', 'lightning_page_header_after' );
-	?>
-
-	<?php
-	//不要do_action
-	//do_action( 'lightning_breadcrumb_before', 'lightning_breadcrumb_before' );
-
-	// breadcrumbポジション無効化
-
-/*
-	if ( apply_filters( 'lightning_is_breadcrumb_position_normal', false, 'breadcrumb_position_normal' ) ) {
-
-		if ( apply_filters( 'lightning_is_breadcrumb', false, 'breadcrumb' ) ) {
-			$vk_breadcrumb      = new VkBreadcrumb();
-			$breadcrumb_options = array(
-				'id_outer'        => 'breadcrumb',
-				'class_outer'     => 'breadcrumb',
-				'class_inner'     => 'container',
-				'class_list'      => 'breadcrumb-list',
-				'class_list_item' => 'breadcrumb-list__item',
-			);
-			$vk_breadcrumb->the_breadcrumb( $breadcrumb_options );
-		}
-	}
-*/
-	//不要do_action
-	//do_action( 'lightning_breadcrumb_after', 'lightning_breadcrumb_after' );
-	?>
-
-<?php // endif; ?>
-
-<?php 
-//不要do_action
-//do_action( 'lightning_site_body_before', 'lightning_site_body_before' );
 ?>
 
 <div class="<?php lightning_the_class_name( 'site-body' ); ?>">
@@ -120,24 +57,5 @@ if ( apply_filters( 'lightning_is_site_header', false, 'site_header' ) ) {
 	<?php do_action( 'lightning_site_body_append', 'lightning_site_body_append' ); ?>
 
 </div><!-- [ /.site-body ] -->
-
-<?php
-//footer-before-widget は削除済
-
-
-//不要do_action
-//do_action( 'lightning_site_footer_before', 'lightning_site_footer_before' );
-
-//フッター を false で読み込まない様にする
-/*
-if ( apply_filters( 'lightning_is_site_footer', false, 'site_footer' ) ) {
-	lightning_get_template_part( 'template-parts/site-footer' );
-}
-*
-*/
-
-//不要do_action
-//do_action( 'lightning_site_footer_after', 'lightning_site_footer_after' );
-?>
 
 <?php lightning_get_template_part( 'footer' ); ?>
